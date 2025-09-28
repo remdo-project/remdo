d350f941fb1d89009d8ad195f74607596849c81a
Piotr Lewalski <piotr@lewalski.pl>
2025-09-28 10:40:23 +0000

    dropping browser tests watch and hocuspocus

--- scripts/hocuspocus.mjs
+++
@@ -1,9 +0,0 @@
-import { Logger } from "@hocuspocus/extension-logger";
-import { Hocuspocus } from "@hocuspocus/server";
-
-const server = new Hocuspocus({
-  extensions: [new Logger()],
-  port: 8080,
-});
-
-server.listen();
