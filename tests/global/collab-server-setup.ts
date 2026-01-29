454036bccd5f9af6f2d2f107ea1e0ddb3eaa871e
Piotr Lewalski <piotr@lewalski.pl>
2026-01-29 16:41:02 +0100

    test: skip collab server setup in docker e2e

--- tests/global/collab-server-setup.ts
+++ tests/global/collab-server-setup.ts
@@ -1,7 +1,13 @@
+import process from 'node:process';
 import { config } from '../../config';
 import { ensureCollabServer } from '../../tools/lib/collab-server-helper';
 
 export default async function collabServerSetup() {
+  // eslint-disable-next-line node/no-process-env -- docker E2E uses env-only flag
+  if (process.env.E2E_DOCKER === 'true') {
+    return;
+  }
+
   if (!config.env.COLLAB_ENABLED) {
     return;
   }
